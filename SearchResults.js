9289b295cf2d9468f398c6218a59867eaa823b7e
txu <txu@godaddy.com>
2016-12-28 17:06:07 -0800

    property view detail

--- SearchResults.js
+++ SearchResults.js
@@ -9,6 +9,8 @@ import {
   Text
 } from 'react-native';
 
+var PropertyView = require('./PropertyView');
+
 var styles = StyleSheet.create({
   thumb: {
     width: 80,
@@ -73,6 +75,12 @@ class SearchResults extends Component {
 
   rowPressed(listerURL) {
     var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   render() {
