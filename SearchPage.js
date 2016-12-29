421e2c04266b6ebf565edbccde6bd8345cec7672
txu <txu@godaddy.com>
2016-12-28 17:16:34 -0800

    add location service

--- SearchPage.js
+++ SearchPage.js
@@ -130,6 +130,21 @@ class SearchPage extends Component {
     this._executeQuery(query);
   }
 
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({ searchString: search });
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+      });
+    });
+  }
+
   render() {
     console.log('Search Page.render');
     var spinner = this.state.isLoading ? ( <ActivityIndicator
@@ -164,7 +179,7 @@ class SearchPage extends Component {
           alignSelf: 'stretch',
           justifyContent: 'center'
          }}
-        underlayColor='#99d9f4'>
+        underlayColor='#99d9f4' onPress={this.onLocationPressed.bind(this)}>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('./Resources/house.png')} style={styles.image}/>
